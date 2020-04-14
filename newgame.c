169e60975fcb18b7c927c79dd423647ca163a2e9
Robert Harmaakivi <robert.harmaakivi@gmail.com>
2020-04-14 12:21:12 +0300

    Added comments and cleaned the code

--- newgame.c
+++ newgame.c
@@ -1,3 +1,45 @@
+/***************************************************************************
+ *   Copyright (C) 2020 by Robert Harmaakivi                               *
+ *   robert.harmaakivi@gmail.com                                           *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+/*********************************************************************
+
+ 1.  NAME
+     Game Of Corona 
+
+ 2.  DESCRIPTION
+     This is a 2020 take on the classic Game of Life, with infection mechanics
+     added to it. There are two map choises, a rectangle, or world map, and
+     the infection mechanics vary a bit depending on the map.
+
+ 3.  VERSIONS
+       Original:
+         14.4.2020 / Robert Harmaakivi
+
+       Version history:
+
+**********************************************************************/
+
+/*-------------------------------------------------------------------*
+*    HEADER FILES                                                    *
+*--------------------------------------------------------------------*/
+
 #include <stdio.h>
 #include <unistd.h>
 #include <ncurses.h>
@@ -7,19 +49,14 @@
 #include <time.h>
 #include <stdbool.h>
 
-void map_choice(void);
-void seed_choice(void);
-void initgame(int mapchoice, int seedchoice);
-void next_step(void);
-int calculate_future(int i, int j);
-void date_and_infection(void);
-void infect(int rounds);
+/*-------------------------------------------------------------------*
+*    GLOBAL VARIABLES AND CONSTANTS                                  *
+*--------------------------------------------------------------------*/
+/* Control flags */
 
-struct cell{
-    int current;
-    int future;
-    bool infection;
-};
+/* Global constants */
+
+/* Global variables */
 
 int y,x,i,j,yMax,xMax;
 int mapchoice = 0;
@@ -28,10 +65,33 @@ int day, month, year;
 int infected = 0;
 int healthy = 0;
 int rounds = 0;
-
-struct cell board[60][200] = {0,0};
 int map[200][200] = {0,0};
 
+/* Global structures */
+
+struct cell{
+    int current;
+    int future;
+    bool infection;
+};
+
+struct cell board[60][200] = {0,0};
+
+/*-------------------------------------------------------------------*
+*    FUNCTION PROTOTYPES                                             *
+*--------------------------------------------------------------------*/
+
+void map_choice(void);
+void seed_choice(void);
+void initgame(int mapchoice, int seedchoice);
+void next_step(void);
+int calculate_future(int i, int j);
+void date_and_infection(void);
+void infect(int rounds);
+
+/*********************************************************************
+*    MAIN PROGRAM                                                    *
+*********************************************************************/
 
 int main (void){
 
@@ -41,19 +101,20 @@ int main (void){
     noecho();
     nodelay(stdscr, true);
 
+    if (has_colors() == FALSE) {
+        endwin();
+        printf("Your terminal does not support color\n");
+        exit(1);
+    }
+
     srand(time(NULL));
 
     start_color();
 
-    //color 1 = life
-    //color 2 = ground
-    //color 3 = water
-    //color 4 = infection
-
-    init_pair(1, COLOR_WHITE, COLOR_GREEN);
-    init_pair(2, COLOR_GREEN, COLOR_GREEN);
-    init_pair(3, COLOR_WHITE, COLOR_BLUE);
-    init_pair(4, COLOR_RED, COLOR_GREEN);
+    init_pair(1, COLOR_WHITE, COLOR_GREEN); //color 1 = life
+    init_pair(2, COLOR_GREEN, COLOR_GREEN); //color 2 = ground
+    init_pair(3, COLOR_WHITE, COLOR_BLUE);  //color 3 = water
+    init_pair(4, COLOR_RED, COLOR_GREEN);   //color 4 = infection
 
 
     getmaxyx(stdscr, yMax, xMax);
@@ -79,12 +140,29 @@ int main (void){
 
     getch();
     endwin();
-}
+
+}   /* end of main */
+
+/*********************************************************************
+*    FUNCTIONS                                                       *
+**********************************************************************/
+
+/*********************************************************************
+	F U N C T I O N    D E S C R I P T I O N
+---------------------------------------------------------------------
+ NAME: map_choice
+ DESCRIPTION: Prints the game title, and lets player choose from the 2
+              maps available
+	Input: void
+	Output: void
+  Used global variables: yMax, xMax, mapchoice
+ REMARKS when using this function: -
+*********************************************************************/
 
 void map_choice(void){
 
-    /* Init the game mode menu */
-    WINDOW * mapwin = newwin(4, 20, yMax-50, xMax/2-10);
+    /* Init the map menu */
+    WINDOW * mapwin = newwin(4, 20, yMax-30, xMax/2-10);
     box(mapwin, 0, 0);
     char *maps[2] = {"Square", "World Map"};
     int option;
@@ -110,7 +188,7 @@ void map_choice(void){
 
     refresh();
 
-    /* Game mode option functionality */
+    /* map option functionality */
 
     while(1){
         for(int o = 0; o < 2; o++){
@@ -153,10 +231,22 @@ void map_choice(void){
     wrefresh(mapwin);
 }
 
+/*********************************************************************
+	F U N C T I O N    D E S C R I P T I O N
+---------------------------------------------------------------------
+ NAME: seed_choice
+ DESCRIPTION: lets the player choose the seed from 2 options, random or
+              predetermined
+	Input: void
+	Output: void
+  Used global variables: yMax, xMax, seedchoice
+ REMARKS when using this function: NOT IN USE FOR THIS VERSION
+*********************************************************************/
+
 void seed_choice(void){
 
-    /* Init the game mode menu */
-    WINDOW * seedwin = newwin(4, 20, yMax-50, xMax/2-10);
+    /* Init the seed choice menu */
+    WINDOW * seedwin = newwin(4, 20, yMax-30, xMax/2-10);
     box(seedwin, 0, 0);
     char *seeds[2] = {"Random", "Predetermined"};
     int option;
@@ -165,7 +255,7 @@ void seed_choice(void){
     keypad(seedwin, true);
 
 
-    /* Game mode option functionality */
+    /* seed option functionality */
 
     while(1){
         for(int o = 0; o < 2; o++){
@@ -208,6 +298,17 @@ void seed_choice(void){
     wrefresh(seedwin);
 }
 
+/*********************************************************************
+	F U N C T I O N    D E S C R I P T I O N
+---------------------------------------------------------------------
+ NAME: initgame
+ DESCRIPTION: initializes the game, depending on the map and seed choises
+	Input: int mapchoice, int seedchoice
+	Output: void
+  Used global variables: mapchoice, seedchoice, i, j, a, 
+ REMARKS when using this function:
+*********************************************************************/
+
 void initgame(int mapchoice, int seedchoice){
 
     clear();
@@ -218,30 +319,29 @@ void initgame(int mapchoice, int seedchoice){
     file_pointer = fopen("worldmaplarge1.txt", "r");
     int a = 0;
 
-    if(seedchoice==1){
-        if(mapchoice==1){
+    if(seedchoice==1){                      //random seed
+        if(mapchoice==1){                   //square map
             for(i=0; i<59; i++){
                 for(j=0; j<200; j++){
-                    map[i][j]=1;
-                    int r = rand()%2;
-                    board[i][j].current = r;                
+                    map[i][j]=1;            //set the whole square as playable
+                    int r = rand()%2;       //50% chance of creating a cell
+                    board[i][j].current = r;
                 }
                 refresh();
-            }
-            
-        } else if (mapchoice==2) {
+            } 
+        } else if (mapchoice==2) {          //world map
 
             while(fgets(line, sizeof line, file_pointer) != NULL){
                 move(a,0);
                 int i=0;
                 for(int j=0; j<sizeof line; j++){
                     if(line[j]=='~'){
-                        map[a][j] = 0;
+                        map[a][j] = 0;      //if map is 0, that means water
                         attron(COLOR_PAIR(3));
                         printw("%c", line[j]);
                         attroff(COLOR_PAIR(3));
                     } else {
-                        map[a][j] = 1;
+                        map[a][j] = 1;      //if map is 1, that means land
                         attron(COLOR_PAIR(2));
                         printw("%c", line[j]);
                         attroff(COLOR_PAIR(2));
@@ -252,46 +352,37 @@ void initgame(int mapchoice, int seedchoice){
                 a++;
             }
 
-        for(i=0; i<60; i++){
-            for(j=0; j<200; j++){
-                board[i][j].infection = false;
-                if(map[i][j]==0){
-                    board[i][j].current = 0;
-                } else if (map[i][j]==1){
-                    int r = rand()%2;
-                    board[i][j].current = r;
-                }
-            }
-        }
-
-        //set random infection
-
-
-/*        for(i=14; i<20; i++){
-            for(j=150; j<170; j++){
-                if(board[i][j].current==1){
-                    if(rand()%100<=50){
-                        board[i][j].infection = true;
+            for(i=0; i<60; i++){
+                for(j=0; j<200; j++){
+                    board[i][j].infection = false; //set infection initially to 0
+                    if(map[i][j]==0){              //cannot live on water
+                        board[i][j].current = 0;
+                    } else if (map[i][j]==1){      //if theres land, cells can be made
+                        int r = rand()%2;
+                        board[i][j].current = r;
                     }
                 }
             }
         }
-*/
-
-
-        }
     }
-
-    if(seedchoice==2){
-
-    }
-
-    /* NEXT STEP */
+    
+//    if(seedchoice==2){}
 
 }
 
+/*********************************************************************
+	F U N C T I O N    D E S C R I P T I O N
+---------------------------------------------------------------------
+ NAME: next_step
+ DESCRIPTION: takes the next step in the game. prints the board according
+              to the current status of cells
+	Input: void
+	Output: void
+  Used global variables: i, j, rounds
+ REMARKS when using this function:
+*********************************************************************/
 
-void next_step(){
+void next_step(void){
 
 
     for(i=0; i<59; i++){
@@ -309,7 +400,7 @@ void next_step(){
         }
     }
     
-
+    //printing the map according to the map, cells and infection
 
     for(i=0; i<59; i++){
         for(j=0; j<200; j++){
@@ -341,15 +432,29 @@ void next_step(){
         }
     }
 
-            infect(rounds);
+    infect(rounds); //set the random infection
 
     rounds++;
 
-    date_and_infection();
+    date_and_infection(); 
 
     refresh();
     
 }
+
+/*********************************************************************
+	F U N C T I O N    D E S C R I P T I O N
+---------------------------------------------------------------------
+ NAME: calculate_future
+ DESCRIPTION: calculates the future status of cells, according to the
+              rules set in game of life. also added the functionality
+              for the infection.
+	Input: int i, int j
+	Output: int
+  Used global variables: i, j, a, b, mapchoice
+ REMARKS when using this function: the infection rates have been set 
+                                   to match the current map options
+*********************************************************************/
 
 int calculate_future(int i, int j){
 
@@ -361,13 +466,13 @@ int calculate_future(int i, int j){
         if(board[i][j].current == 0){ //if the current cell is empty
             for(a=-1; a<2; a++){
                 for(b=-1; b<2; b++){ //going through all the nine points around the point
-                    if(board[i+a][j+b].infection == true){
+                    if(board[i+a][j+b].infection == true){ //checking if neighbors are infected
                         if(mapchoice==1){
-                            if(rand()%100<25){
+                            if(rand()%100<25){ //lower spread chance in square map
                                 board[i][j].infection = true;
                             }
                         } else {
-                            if(rand()%100<50){
+                            if(rand()%100<50){ //higher chance in world map
                                 board[i][j].infection = true;
                             }
                         }
@@ -387,13 +492,13 @@ int calculate_future(int i, int j){
             neighbors--;              //deducting itself from the neigbors
             for(a=-1; a<2; a++){
                 for(b=-1; b<2; b++){
-                    if(board[i+a][j+b].infection == true){
+                    if(board[i+a][j+b].infection == true){ //checking if neighbors are infected
                         if(mapchoice==1){
-                            if(rand()%100<20){
+                            if(rand()%100<20){  //lower spread chance in square map
                                 board[i][j].infection = true;
                             }
                         } else {
-                            if(rand()%100<50){
+                            if(rand()%100<50){ //higher chance in world map
                                 board[i][j].infection = true;
                             }
                         }
@@ -411,6 +516,18 @@ int calculate_future(int i, int j){
         return 0;
     }
 }
+
+/*********************************************************************
+	F U N C T I O N    D E S C R I P T I O N
+---------------------------------------------------------------------
+ NAME: date_and_infection
+ DESCRIPTION: keeps track, and prints the current date, and infections
+	Input: void
+	Output: void
+  Used global variables: i, j, infected, healthy
+ REMARKS when using this function: has the calculator for healthy, but
+                                   its not used in this version
+*********************************************************************/
 
 void date_and_infection(void){
 
@@ -435,8 +552,7 @@ void date_and_infection(void){
 
     currently_infected = infected - currently_infected;
 
-
-    if(infected<0){
+    if(infected < 0){
         infected = 0;
     }
 
@@ -444,10 +560,10 @@ void date_and_infection(void){
 
     mvprintw(0,0, "%d.%d.%d", day, month, year);
     mvprintw(1,0, "Infected (cumulative): %d", infected);
-//    mvprintw(2,0, "rounds: %d", rounds);
-//    mvprintw(3,0, "healthy c: %d", healthy);
     mvprintw(2,0, "Currently infected: %d", currently_infected);
     mvprintw(4,0,"Press x to exit");
+//    mvprintw(5,0, "rounds: %d", rounds);
+//    mvprintw(6,0, "healthy c: %d", healthy);
 
     day++;
 
@@ -469,28 +585,44 @@ void date_and_infection(void){
 
 }
 
+/*********************************************************************
+	F U N C T I O N    D E S C R I P T I O N
+---------------------------------------------------------------------
+ NAME: infect
+ DESCRIPTION: sets infections depending on the round
+	Input: rounds
+	Output: void
+  Used global variables: i, j, rounds
+ REMARKS when using this function: set to use with the world map.
+                                   infection starts in china, and after
+                                   30 rounds, tries to infect the USA
+*********************************************************************/
+
 void infect(int rounds){
 
-if(rounds<=1){
-    for(i=14; i<20; i++){
-        for(j=150; j<170; j++){
-            if(board[i][j].current==1){
-                if(rand()%100<=50){
-                    board[i][j].infection = true;
+    if(rounds<=1){
+        for(i=14; i<20; i++){
+            for(j=150; j<170; j++){
+                if(board[i][j].current==1){
+                    if(rand()%100<=50){
+                        board[i][j].infection = true;
+                    }
                 }
             }
         }
-    }
-} else if (rounds > 30 && rounds < 40){
-    for(i=18; i<23; i++){
-        for(j=32; j<48; j++){
-            if(board[i][j].current==1){
-                if(rand()%100<=50){
-                    board[i][j].infection = true;
+    } else if (rounds > 30 && rounds < 40){
+        for(i=18; i<23; i++){
+            for(j=32; j<48; j++){
+                if(board[i][j].current==1){
+                    if(rand()%100<=50){
+                        board[i][j].infection = true;
+                    }
                 }
             }
         }
     }
 }
 
-}
+
+/*********************************************************************
+*********************************************************************/
